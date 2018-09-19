a70b01523266a19c6d13b4e1d5aba95f06a0f072
Mjamai <mjf.php@gmail.com>
2018-09-19 17:23:49 +0200

    Creation d'un jar executable

---
+++ bin/Manifest.ml
@@ -0,0 +1,9 @@
+Manifest-Version: 1.0
+Ant-Version: Apache Ant 1.10.5
+Created-By: 1.8.0_144-b01 (Oracle Corporation)
+main-class: fr.ubo.tetris.Tetris
+implementation-version: 1.0
+build-by: JAMAI
+Class-Path: /Users/mjfphp/eclipse-workspace/Tetris-/lib/commons-lang3-
+ 3.5.jar
+
